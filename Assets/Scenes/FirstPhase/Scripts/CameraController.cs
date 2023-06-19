72a23a938ab440893bd03c83cbfba7a3826b72ea
AnthoniBortolotto <antho@outlook.com.br>
2023-06-19 16:33:04 -0300

    Player move the camera added

--- Assets/Scenes/FirstPhase/Scripts/CameraController.cs
+++ Assets/Scenes/FirstPhase/Scripts/CameraController.cs
@@ -1,15 +1,13 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform target;
-    public float smoothSpeed = 0.125f;
-    public Vector3 offset;
-
+   [SerializeField]
+    private Transform target;
     private void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
-        transform.position = smoothedPosition;
+        transform.position = new Vector3(target.position.x, target.position.y + 2, transform.position.z);
     }
 }
